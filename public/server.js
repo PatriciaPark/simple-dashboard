a11f89db71466e7a12712d8552d4162cc6ea64f1
PATRICIA PARK <pyjee8@google.com>
2022-05-26 21:28:14 +0800

    simple route

--- public/server.js
+++ public/server.js
@@ -10,11 +10,9 @@ app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
-// app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cookieSession({
@@ -31,20 +29,14 @@ const Role = db.role;
 
 // db.sequelize.sync();
 
-// force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
-
 // simple route
-app.get("/", (req, res) => {
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.get("/*", (req, res) => {
   res.sendFile('./index.html', {root: __dirname })
   // res.json({ message: "Welcome to simple-dashboard application." });
 });
 
 // routes
-// require("./routes/route")(app);
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 require("./routes/tutorial.routes")(app);
@@ -55,20 +47,3 @@ console.log(PORT);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "user",
-//   });
-
-//   Role.create({
-//     id: 2,
-//     name: "moderator",
-//   });
-
-//   Role.create({
-//     id: 3,
-//     name: "admin",
-//   });
-// }
