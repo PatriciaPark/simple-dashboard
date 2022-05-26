b773060680110b20c13de286ab8e457248e49afc
PATRICIA PARK <pyjee8@google.com>
2022-05-26 19:23:38 +0800

    DB info - heroku mysql db

--- public/config/db.config.js
+++ public/config/db.config.js
@@ -1,21 +1,8 @@
-// module.exports = {
-//   HOST: '127.0.0.1',
-//   USER: 'root',
-//   PASSWORD: 'dudwlsla',
-//   DB: 'simple_dashboard',
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
 module.exports = {
-  HOST: 'pyjee8.dothome.co.kr',
-  USER: 'pyjee8',
-  PASSWORD: 'Q1w2e3r4!',
-  DB: 'simple_dashboard',
+  HOST: process.env.DATABASE_HOST,
+  USER: process.env.DATABASE_USER,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  DB: process.env.DATABASE,
   dialect: "mysql",
   pool: {
     max: 5,
@@ -24,9 +11,3 @@ module.exports = {
     idle: 10000
   }
 };
-// module.exports = {
-//     HOST: '127.0.0.1',
-//     USER: 'root',
-//     PASSWORD: 'dudwlsla',
-//     DB: 'simple_dashboard',
-//   };
