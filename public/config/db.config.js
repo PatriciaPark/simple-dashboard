86b5f24dddbb69ec404ea3d2688dc37617ccde0f
PATRICIA PARK <pyjee8@google.com>
2022-05-26 19:03:44 +0800

    DB info

--- public/config/db.config.js
+++ public/config/db.config.js
@@ -1,7 +1,20 @@
+// module.exports = {
+//   HOST: '127.0.0.1',
+//   USER: 'root',
+//   PASSWORD: 'dudwlsla',
+//   DB: 'simple_dashboard',
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
 module.exports = {
-  HOST: '127.0.0.1',
-  USER: 'root',
-  PASSWORD: 'dudwlsla',
+  HOST: 'pyjee8.dothome.co.kr',
+  USER: 'pyjee8',
+  PASSWORD: 'Q1w2e3r4!',
   DB: 'simple_dashboard',
   dialect: "mysql",
   pool: {
