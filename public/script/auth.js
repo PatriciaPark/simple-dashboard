8f6bf3cd47023ffbcdf24d2ca2acac23fd05a81b
PATRICIA PARK <pyjee8@google.com>
2022-05-27 11:41:37 +0800

    auth.js : sign in email verification

--- public/script/auth.js
+++ public/script/auth.js
@@ -3,6 +3,22 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 const uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult(authResult, redirectUrl) {
+      // update database - email verification(emailVerification= 0 -> 1)
+      var data = { email: email };
+      fetch('/api/users/verificationData', {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+          })
+          .then((response) => response.json())
+          .then((data) => {
+              console.log('Success:', data);
+          })
+          .catch((error) => {
+              console.error('Fail:', error);
+      });
       console.log("Auth Result: " + authResult);
       return true;
     },
