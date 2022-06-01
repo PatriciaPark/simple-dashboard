9127f5a8dc0f1be86fd02205b3af3e1264086d3b
PATRICIA PARK <pyjee8@google.com>
2022-06-01 14:11:49 +0800

    dashboard ejs path

--- public/server.js
+++ public/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-// const path = require('path');
+const path = require('path');
 const app = express();
 
 // set the view engine to ejs
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cors());
