d32868f65e0f615694e6ea60073e172f0a6febe6
PATRICIA PARK <pyjee8@google.com>
2022-06-09 20:44:45 +0800

    auth email

--- public/controllers/auth.controller.js
+++ public/controllers/auth.controller.js
@@ -71,11 +71,11 @@ exports.getAuth = (req, res) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Not found User with email ${req.params.email}.`
+          message: `Not found Verified User with email ${req.params.email}.`
         });
       } else {
         res.status(500).send({
-          message: "Error retrieving User with email " + req.params.email
+          message: "Error retrieving Verified User with email " + req.params.email
         });
       }
     } else res.send(data);
