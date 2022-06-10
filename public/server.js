7ae765a2a609bf684ccdc2161eb67418a6b68197
PATRICIA PARK <pyjee8@google.com>
2022-06-10 15:28:22 +0800

    path public

--- public/server.js
+++ public/server.js
@@ -60,11 +60,6 @@ require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 require("./routes/tutorial.routes")(app);
 
-// The 404 Route
-app.use(function(req,res){
-  res.status(404).sendFile('./404.html');
-});
-
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
