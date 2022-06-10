5a43f69f62e07c5275978cf013c98e5f95d5f953
PATRICIA PARK <pyjee8@google.com>
2022-06-10 14:25:58 +0800

    redirect server

--- public/server.js
+++ public/server.js
@@ -54,6 +54,10 @@ app.get("/", (req, res) => {
   res.sendFile('./index.html', {root: __dirname })
   // res.json({ message: "Welcome to simple-dashboard application." });
 });
+// The 404 Route
+app.use(function(req,res){
+  res.status(404).sendFile('./404.html');
+});
 
 // routes
 require("./routes/auth.routes")(app);
