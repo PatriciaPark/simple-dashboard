2c5171db9cc46873a78f424d03402d79f63901f5
PATRICIA PARK <pyjee8@google.com>
2022-06-11 21:57:41 +0800

    remove session/cookie

---
+++ public/routes/page.routes.js
@@ -0,0 +1,44 @@
+module.exports = app => {
+  var router = require("express").Router();
+  // dashboard
+  router.get("/dashboard", function(req, res) {
+    if(req.session.user){
+      sessionStorage.setItem('userSIEmail', req.session.user.email);
+      res.sendFile(__dirname + "/views/dashboard.html");
+    }else{
+      res.sendFile(__dirname + "/index.html");
+    }
+  });
+  // editInfo
+  router.get("/editInfo", function(req, res) {
+    if(req.session.user){
+      res.sendFile(__dirname + "/views/editInfo.html");
+    }else{
+      res.sendFile(__dirname + "/index.html");
+    }
+  });
+  // resetPwd
+  router.get("/resetPwd", function(req, res) {
+    if(req.session.user){
+      res.sendFile(__dirname + "/views/resetPwd.html");
+    }else{
+      res.sendFile(__dirname + "/index.html");
+    }
+  });
+  // email_verification
+  router.get("/email_verification", function(req, res) {
+    if(req.session.user){
+      res.sendFile(__dirname + "/views/email_verification.html");
+    }else{
+      res.sendFile(__dirname + "/index.html");
+    }
+  });
+  // logout
+  router.get("/logout", function(req, res) {
+    // Clearing the cookie
+    req.session = null
+    // res.clearCookie("pyjee8-session",{path:"/",domain:"https://simple-dashboard-pyjee8.herokuapp.com/"});
+    res.redirect("/");
+  });
+  app.use('/', router);
+};
--- public/server.js
+++ public/server.js
@@ -97,48 +97,9 @@ app.get("/", (req, res) => {
   // res.json({ message: "Welcome to simple-dashboard application." });
 });
 
-// dashboard
-app.get("/dashboard", function(req, res) {
-  if(req.session.user){
-    res.sendFile(__dirname + "/views/dashboard.html");
-  }else{
-    res.sendFile(__dirname + "/index.html");
-  }
-});
-// editInfo
-app.get("/editInfo", function(req, res) {
-  if(req.session.user){
-    res.sendFile(__dirname + "/views/editInfo.html");
-  }else{
-    res.sendFile(__dirname + "/index.html");
-  }
-});
-// resetPwd
-app.get("/resetPwd", function(req, res) {
-  if(req.session.user){
-    res.sendFile(__dirname + "/views/resetPwd.html");
-  }else{
-    res.sendFile(__dirname + "/index.html");
-  }
-});
-// email_verification
-app.get("/email_verification", function(req, res) {
-  if(req.session.user){
-    res.sendFile(__dirname + "/views/email_verification.html");
-  }else{
-    res.sendFile(__dirname + "/index.html");
-  }
-});
-// logout
-app.get("/logout", function(req, res) {
-  // Clearing the cookie
-  req.session = null
-  // res.clearCookie("pyjee8-session",{path:"/",domain:"https://simple-dashboard-pyjee8.herokuapp.com/"});
-  res.redirect("/");
-});
-
 // routes
 require("./routes/auth.routes")(app);
+require("./routes/page.routes")(app);
 require("./routes/user.routes")(app);
 require("./routes/tutorial.routes")(app);
 
