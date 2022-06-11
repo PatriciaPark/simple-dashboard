c4f2ba8b9c65c5741cf919f203fb9c91da2ca9bc
PATRICIA PARK <pyjee8@google.com>
2022-06-11 23:27:34 +0800

    maintain session

--- public/script/email.js
+++ public/script/email.js
@@ -29,7 +29,6 @@ function emailLinkSend(){
       // The link was successfully sent. Inform the user.
       // Save the email locally so you don't need to ask the user for it again
       window.localStorage.setItem('emailForSignIn', email);
-      document.cookie = "sessionEmail="+email;
       alert("Send Email Successfully");
     })
     .catch((error) => {
