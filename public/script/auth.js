7655627ccc8773b8d80b7073268e9d809e02ac5b
PATRICIA PARK <pyjee8@google.com>
2022-06-12 00:37:39 +0800

    auth.js

--- public/script/auth.js
+++ public/script/auth.js
@@ -4,51 +4,6 @@ const uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult(authResult, redirectUrl) {
       // User successfully signed in
-      
-      const user = firebase.auth().currentUser;
-      if (user !== null) {
-        // The user object has basic properties such as display name, email, etc.
-        const displayName = user.displayName;
-        const email = user.email;
-
-        fetch('/api/users/'+ email)
-        .then((response) => response.json())
-        .then((data) => {
-          if(data.message) {
-
-          } else {
-            fetch('/api/users/create', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username: displayName, email: email, password:""}),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                if(data.message){
-                    // "Failed! Email is already in use!"
-                    alert(data.message);
-                } else {
-                    // Signup Successful
-                    sessionStorage.setItem('userSIEmail', userEmail);
-                }
-                // console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Fail:', error);
-            });
-          }
-        })
-        .catch((error) => {
-          console.error('Fail to Get User:', error);
-        });
-        // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getIdToken() instead.
-        const uid = user.uid;
-      }
-
       console.log("Auth Signin successful");
       return true;
     },
